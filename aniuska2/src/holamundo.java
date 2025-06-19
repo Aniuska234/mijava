3c172daf0978496d131bd89c1a8e8fa8cd29236a
Aniuska Largaespada <aniuska.almendares871@std.uni.edu.ni>
2025-06-19 12:02:07 -0600

    Agrego multiplicación de a y b

--- aniuska2/src/holamundo.java
+++ aniuska2/src/holamundo.java
@@ -1,13 +1,17 @@
 public class holamundo {
     public static void main(String[] args) throws Exception {
         String nombre = "Estudiante"
-        int a = 5
-        int b = 3
+        int a = 10
+        int b = ;
         int suma = a + b
+        int multiplicacion = a * b  
 
         System.out.println("Hola" + nombre "!");
 
         System.out.println("La suma de"+ a + "+" + b + "es" + suma);
+
+        System.out.println("La multiplicación de " + a + " * " + b + " es: " + multiplicacion);
+
 
     }
 }
